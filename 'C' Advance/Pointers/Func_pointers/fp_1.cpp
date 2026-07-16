95fb9e22a7afe62d7d34d6f0699dd7d9e80d5527
riddhimamathur <riddhimamathur2108@gmail.com>
2026-07-16 10:22:15 +0530

    updated

---
+++ 'C' Advance/Pointers/Func_pointers/fp_1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int sum (int *p, int n)
+{
+    int add = 0;
+    for (int i=0; i<n; i++)
+    {
+            add += *(p + i);
+        }
+    return add;
+};
+    int main()
+    {
+        int a[] = {1,2,3,4,5,6};
+        int n = sizeof(a)/sizeof(a[0]);
+        cout << "sum is : " << sum (a,n);
+        return 0;
+    }
+
+    //----------------------Or-----------------------
+
+   /*int main()
+    {
+        int a[] = {1,2,3,4,5,6};
+        int *p=a;
+        int sum = 0;
+        while (p < a + 6)
+        {
+            sum += *p;
+            p++;
+        }
+        cout << "sum is : " << sum;
+    }
+        */
+
