21257ab0d8cf62f30e50af82ff43c436d6b06c40
riddhimamathur <riddhimamathur2108@gmail.com>
2026-07-17 10:25:11 +0530

    all are updated

--- 'C' Advance/Pointers/linearSearch.cpp
+++ 'C' Advance/Pointers/linearSearch.cpp
@@ -3,12 +3,12 @@
 int main() {
     int arr[] = {10, 25, 30, 45, 50};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int key = 45;   // जिस element को ढूँढना है
-    int *p = arr;   // pointer to array
+    int key = 45;   
+    int *p = arr;  
     int found = -1;
 
     for (int i = 0; i < n; i++) {
-        if (*(p + i) == key) {   // pointer से access
+        if (*(p + i) == key) {   
             found = i;
             break;
         }
