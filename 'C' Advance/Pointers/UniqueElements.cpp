9558b4e95f1e241dec1234bb8601a895f2c8c060
riddhimamathur <riddhimamathur2108@gmail.com>
2026-07-17 10:49:20 +0530

    new one added

---
+++ 'C' Advance/Pointers/UniqueElements.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 3, 2, 3, 4, 5, 2, 6};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int *p = arr;
+
+    cout << "Unique elements: ";
+    for (int i = 0; i < n; i++) {
+        bool isDuplicate = false;
+
+        // बाकी elements से compare करो
+        for (int j = 0; j < n; j++) {
+            if (i != j && *(p+i) == *(p+j)) {
+                isDuplicate = true;
+                break;
+            }
+        }
+
+        if (!isDuplicate) {
+            cout << *(p+i) << " ";
+        }
+    }
+    return 0;
+}
