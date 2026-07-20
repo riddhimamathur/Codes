1dc98fbfaa876b0db495de7b1d76a996b4e77d39
riddhimamathur <riddhimamathur2108@gmail.com>
2026-07-20 10:29:01 +0530

    updated

--- 'C' Advance/Pointers/Matrix/SumOfEachColumn.cpp
+++ 'C' Advance/Pointers/Matrix/SumOfEachColumn.cpp
@@ -6,20 +6,18 @@ int main() {
     cout << "Enter rows and columns: ";
     cin >> m >> n;
 
-    int *ptr = new int[m*n];   // 1D pointer for matrix
+    int *ptr = new int[m*n];   
 
     cout << "Enter " << m*n << " elements:\n";
     for(int i=0; i<m; i++) {
         for(int j=0; j<n; j++) {
-            cin >> *(ptr + i*n + j);   // pointer arithmetic for input
+            cin >> *(ptr + i*n + j);   
         }
     }
-
-    // Column sums
     for(int j=0; j<n; j++) {
         int colSum = 0;
         for(int i=0; i<m; i++) {
-            colSum += *(ptr + i*n + j);   // ith row, jth column
+            colSum += *(ptr + i*n + j);   /
         }
         cout << "Sum of column " << j+1 << " = " << colSum << endl;
     }
--- 'C' Advance/Pointers/Matrix/SumOfEveryRow.cpp
+++ 'C' Advance/Pointers/Matrix/SumOfEveryRow.cpp
@@ -6,24 +6,20 @@ int main() {
     cout << "Enter rows and columns: ";
     cin >> m >> n;
 
-    int *ptr = new int[m*n];   // 1D pointer for matrix
+    int *ptr = new int[m*n];   
 
     cout << "Enter " << m*n << " elements:\n";
     for(int i=0; i<m; i++) {
         for(int j=0; j<n; j++) {
-            cin >> *(ptr + i*n + j);   // pointer arithmetic for input
+            cin >> *(ptr + i*n + j);   
         }
     }
-
-    // Row sums
     for(int i=0; i<m; i++) {
         int rowSum = 0;
         for(int j=0; j<n; j++) {
-            rowSum += *(ptr + i*n + j);   // ith row, jth column
+            rowSum += *(ptr + i*n + j);  
         }
         cout << "Sum of row " << i+1 << " = " << rowSum << endl;
     }
-
     delete[] ptr;
 }
-
