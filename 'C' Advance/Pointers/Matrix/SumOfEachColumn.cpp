5bcd0be6eb9fa1171a3a7c2beab9b97257d3c156
riddhimamathur <riddhimamathur2108@gmail.com>
2026-07-20 10:31:22 +0530

    updates done

--- 'C' Advance/Pointers/Matrix/SumOfEachColumn.cpp
+++ 'C' Advance/Pointers/Matrix/SumOfEachColumn.cpp
@@ -6,18 +6,20 @@ int main() {
     cout << "Enter rows and columns: ";
     cin >> m >> n;
 
-    int *ptr = new int[m*n];   
+    int *ptr = new int[m*n];   // 1D pointer for matrix
 
     cout << "Enter " << m*n << " elements:\n";
     for(int i=0; i<m; i++) {
         for(int j=0; j<n; j++) {
-            cin >> *(ptr + i*n + j);   
+            cin >> *(ptr + i*n + j);   // pointer arithmetic for input
         }
     }
+
+    // Column sums
     for(int j=0; j<n; j++) {
         int colSum = 0;
         for(int i=0; i<m; i++) {
-            colSum += *(ptr + i*n + j);   /
+            colSum += *(ptr + i*n + j);   // ith row, jth column
         }
         cout << "Sum of column " << j+1 << " = " << colSum << endl;
     }
