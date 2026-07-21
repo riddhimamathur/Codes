d33a142051abf4f6ab67e44669d29e239eae16e0
riddhimamathur <riddhimamathur2108@gmail.com>
2026-07-21 10:36:43 +0530

    added new file

---
+++ 'C' Advance/Pointers/Matrix/ReverseEveryRow.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, n;
+    cout << "Enter rows and columns number : ";
+    cin >> m >> n;
+
+    int *A = new int[m*n];
+    cout << "Enter matrix elements:\n";
+    for(int i=0; i<m; i++)
+        for(int j=0; j<n; j++)
+            cin >> *(A + i*n + j);
+
+    for(int i=0; i<m; i++) {
+        for(int j=0; j<n/2; j++) {
+            int temp = *(A + i*n + j);
+            *(A + i*n + j) = *(A + i*n + (n-1-j));
+            *(A + i*n + (n-1-j)) = temp;
+        }
+    }
+
+  cout <<"\nnew matrix = ";
+
+    for(int i=0; i<m; i++) {
+        for(int j=0; j<n; j++)
+            cout << *(A + i*n + j) << " ";
+        cout << endl;
+    }
+    return 0;
+}
