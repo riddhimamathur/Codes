c98ad448d7194b5425484cded9bbd5cb11fd5dc3
riddhimamathur <riddhimamathur2108@gmail.com>
2026-07-21 10:47:48 +0530

    file added

--- 'C' Advance/Pointers/Matrix/ReverseEveryRow.cpp
+++ 'C' Advance/Pointers/Matrix/ReverseEveryRow.cpp
@@ -29,3 +29,38 @@ int main() {
     }
     return 0;
 }
+
+/* for column reverse 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, n;
+    cout << "Enter rows and columns: ";
+    cin >> m >> n;
+
+    int *A = new int[m*n];
+    cout << "Enter matrix elements:\n";
+    for(int i=0; i<m; i++)
+        for(int j=0; j<n; j++)
+            cin >> *(A + i*n + j);
+
+    for(int j=0; j<n; j++) {
+        for(int i=0; i<m/2; i++) {
+            int temp = *(A + i*n + j);
+            *(A + i*n + j) = *(A + (m-1-i)*n + j);
+            *(A + (m-1-i)*n + j) = temp;
+        }
+    }
+
+    cout << "Matrix after reversing each column:\n";
+    for(int i=0; i<m; i++) {
+        for(int j=0; j<n; j++)
+            cout << *(A + i*n + j) << " ";
+        cout << endl;
+    }
+
+    delete[] A;
+}
+*/
---
+++ 'C' Advance/Pointers/Matrix/sum_of_main_diagonal.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of square matrix: ";
+    cin >> n;
+
+    int *A = new int[n*n];
+    cout << "Enter elements:\n";
+    for(int i=0; i<n; i++)
+        for(int j=0; j<n; j++)
+            cin >> *(A + i*n + j);
+            
+    int sum = 0;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            if(i == j) {   
+                sum += *(A + i*n + j);
+            }
+        }
+    }
+
+    cout << "\nSum of main diagonal = " << sum << endl;
+
+    delete[] A;
+}
