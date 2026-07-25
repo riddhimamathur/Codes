70ca4df24d533a7236b77a24446794f152225d38
riddhimamathur <riddhimamathur2108@gmail.com>
2026-07-25 10:15:15 +0530

    new files added

---
+++ 'C' Advance/Dynamic Memory Allocation/14.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int id;
+    char name[50];
+    float price;
+} Product;
+
+void display(Product *arr, int size) {
+    printf("\nProduct List:\n");
+    for (int i = 0; i < size; i++) {
+        printf("ID: %d | Name: %s | Price: %.2f\n", arr[i].id, arr[i].name, arr[i].price);
+    }
+}
+
+int main() {
+    int n = 3; 
+    Product *arr = (Product*) calloc(n, sizeof(Product));
+    arr[0].id = 1; strcpy(arr[0].name, "Shoes"); arr[0].price = 2000.00;
+    arr[1].id = 2; strcpy(arr[1].name, "Heels"); arr[1].price = 2500.00;
+    arr[2].id = 3; strcpy(arr[2].name, "Gloss"); arr[2].price = 1800.75;
+
+    display(arr, n);
+    int delId = 2;
+    for (int i = 0; i < n; i++) {
+        if (arr[i].id == delId) {
+            for (int j = i; j < n - 1; j++) {
+                arr[j] = arr[j + 1]; 
+            }
+            n--; 
+            arr = (Product*) realloc(arr, n * sizeof(Product));  
+            break;
+        }
+    }
+
+    printf("\nAfter deletion:\n");
+    display(arr, n);
+
+    free(arr);  
+    return 0;
+}
---
+++ 'C' Advance/Dynamic Memory Allocation/16.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int m, n;
+    printf("Enter rows number and column number : ");
+    scanf("%d %d", &m, &n);
+
+    int *matrix = (int*) malloc(m * n * sizeof(int));
+
+    printf("Enter elements of the matrix A :\n");
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", (matrix + i*n + j));
+
+    printf("\nMatrix A is :\n");
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++)
+            printf("%d ", *(matrix + i*n + j));
+        printf("\n");
+    }
+    printf("\nRow sums:\n");
+    for (int i = 0; i < m; i++) {
+        int sum = 0;
+        for (int j = 0; j < n; j++)
+            sum += *(matrix + i*n + j);
+        printf("Row %d sum = %d\n", i+1, sum);
+    }
+    printf("\nColumn sums:\n");
+    for (int j = 0; j < n; j++) {
+        int sum = 0;
+        for (int i = 0; i < m; i++)
+            sum += *(matrix + i*n + j);
+        printf("Column %d sum = %d\n", j+1, sum);
+    }
+
+    if (m == n) {
+        int dSum = 0;
+        for (int i = 0; i < m; i++)
+            dSum += *(matrix + i*n + i);
+        printf("\nDiagonal sum = %d\n", dSum);
+    }
+
+    free(matrix);
+    return 0;
+}
