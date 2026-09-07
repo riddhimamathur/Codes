a2f5a45e16465f578c604b73f76630363b2cf606
riddhimamathur <riddhimamathur2108@gmail.com>
2026-09-07 21:55:39 +0530

    changes made in code of sortings

--- Sorts/quicksort.cpp
+++ Sorts/quicksort.cpp
@@ -16,7 +16,7 @@ void swap(int &x, int &y) {
 }
 
 int partition(int arr[], int low, int high) {
-    int pivot = arr[high];  // choose last element as pivot
+    int pivot = arr[high];  
     int i = low - 1;
 
     for (int j = low; j < high; j++) {
@@ -32,8 +32,6 @@ int partition(int arr[], int low, int high) {
 void quickSort(int arr[], int low, int high) {
     if (low < high) {
         int pi = partition(arr, low, high);
-
-        // Recursively sort elements before and after partition
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
