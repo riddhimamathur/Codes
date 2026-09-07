72f07f8f6090833f421bd267c6258c6c576b3bd0
riddhimamathur <riddhimamathur2108@gmail.com>
2026-09-07 21:08:39 +0530

    new code

---
+++ Sorts/quicksort.cpp
@@ -0,0 +1,65 @@
+// QUICK SORT 
+/*QUICK SORT : Works over divide and conquer approach.
+   1) Choose a pivot element from the array.  {last element is commonly used as pivot}  
+   2) Partition the array into two sub-arrays: elements less than the pivot and elements 
+       greater than the pivot. (left m chote or right m bade)
+   */
+  
+    
+#include <iostream>
+using namespace std;
+
+void swap(int &x, int &y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];  // choose last element as pivot
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        // Recursively sort elements before and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    if (n <= 0) return 0;
+
+    int *arr = new int[n];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    delete[] arr;
+    return 0;
+}
